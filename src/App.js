dd317c182e5fc3f8aacdd489eb161d9b10a7e653
Masoud Moniri <masoudmoniri10@gmail.com>
2022-08-29 19:38:16 +1000

    added REACT_APP to config vars

--- src/App.js
+++ src/App.js
@@ -2,17 +2,10 @@ import React from "react";
 import InputForm from "./InputForm.js";
 import Giphy from "./Giphy.js";
 const apiKey = process.env.REACT_APP_GIPHY_API_KEY;
-const againKey = process.env.REACT_APP_GIPHY_API_KEY;
-const SITE = process.env.SITE;
-const aa = process.env.AA;
-const bb = process.env.BB;
+const SITE = process.env.REACT_APP_SITE;
 function App() {
   console.log("apiKey=", apiKey);
   console.log("SITE=", SITE);
-  console.log("AA=", aa);
-  console.log("BB=", bb);
-  console.log("againKey=", againKey);
-
   const [giphyUrl, setGiphyUrl] = React.useState(null);
   const [error, setError] = React.useState(null);
   const [isLoading, setIsLoading] = React.useState(false);
@@ -40,7 +33,7 @@ function App() {
       setError("Connection error when fetching");
     };
 
-    let url = `https://api.giphy.com/v1/gifs/translate?api_key=${apiKey}&s=${
+    let url = `https://api.giphy.com/v1/gifs/translate?api_key=${"apiKey"}&s=${
       document.getElementById("text").value
     }&weirdness=0`;
     fetch(url).then(success_callback, failure_callback);
